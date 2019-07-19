c52f33a1e73279ffe46610a0f9752135de5d96f3
Michael Lese <michael.lese@cincpro.com>
2019-07-19 18:27:04 -0400

    stack example

---
+++ StackExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExample
+{
+
+    public interface IStack
+    {
+        void push(int val);
+        int pop();
+        int peek();
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Stack: IStack
+    {
+        public Stack() { }
+
+        public void push(int val)
+        {
+
+        }
+
+        public int pop()
+        {
+            return 0;
+        }
+
+        public int peek()
+        {
+            return 0;
+        }
+    }
+}
---
+++ StackExampleTests/StackTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExample.Tests
+{
+    [TestClass()]
+    public class StackTests
+    {
+
+        Stack stack;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            stack = new Stack();
+        }
+
+        [TestMethod()]
+        public void pushTest()
+        {
+            stack.push(1);
+            Assert.AreEqual(1, stack.peek());
+        }
+
+        [TestMethod()]
+        public void popTest()
+        {
+            stack.push(1);
+            stack.push(2);
+            Assert.AreEqual(2, stack.pop());
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void pushMultipleTest()
+        {
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+
+            Assert.AreEqual(3, stack.peek());
+        }
+
+        [TestMethod()]
+        public void popMultipleTest()
+        {
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+
+            stack.pop();
+            Assert.AreEqual(2, stack.pop());
+        }
+    }
+}
